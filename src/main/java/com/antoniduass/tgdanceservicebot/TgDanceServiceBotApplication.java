85677f17cac9f164263084133a5ee9f83da70c93
Anton Bukhanets <antoniduass@gmail.com>
2024-12-10 23:26:33 +0300

    First commit

---
+++ src/main/java/com/antoniduass/tgdanceservicebot/TgDanceServiceBotApplication.java
@@ -0,0 +1,13 @@
+package com.antoniduass.tgdanceservicebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgDanceServiceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgDanceServiceBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/antoniduass/tgdanceservicebot/TgDanceServiceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniduass.tgdanceservicebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgDanceServiceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
